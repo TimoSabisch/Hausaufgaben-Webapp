d343fd54ab547800c44fd735a2d6253cd2573815
Timo <88710671+TimoSabisch@users.noreply.github.com>
2021-08-12 13:22:11 +0200

    Created initial views

--- Webapp/Hausaufgaben/views.py
+++ Webapp/Hausaufgaben/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, redirect
 
-# Create your views here.
+
+def index(request):
+    user_id = request.session.get("user_id", 1)  # TODO: change default id to -1
+
+    return redirect("home" if user_id != -1 else "login")
+
+
+def home(request):
+
+    return HttpResponse("HOME")
+
+
+def login(request):
+
+    return HttpResponse("LOGIN")
