94541e91d98cc1785c4dcc2fdd4772e98d94e648
Timo <88710671+TimoSabisch@users.noreply.github.com>
2021-08-12 09:08:49 +0200

    Set timezone to CEST Configured Database to use MySQL

--- Webapp/Webapp/settings.py
+++ Webapp/Webapp/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'Webapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'webappdb',
+        'USER': 'dbadmin',
+        'PASSWORD': 'dbadmin1234',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -105,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'CEST'
 
 USE_I18N = True
 
