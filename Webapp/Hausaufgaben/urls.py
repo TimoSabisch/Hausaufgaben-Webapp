56aca10d34a051579d3e13a30d1acf60b5935e24
Timo <88710671+TimoSabisch@users.noreply.github.com>
2021-08-13 14:55:42 +0200

    Revert "Added urls for views"
    This reverts commit 2dccad2a730562cbc9d7ec0062944a520acd8540.

--- Webapp/Hausaufgaben/urls.py
+++ Webapp/Hausaufgaben/urls.py
@@ -5,8 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('home', views.home, name='home'),
-    path('weekview', views.week_view, name='weekview'),
-    path('entryview', views.entry_view, name='entryview'),
-    path('dayview', views.day_view, name='dayview'),
     path('login', views.login, name='login'),
 ]
