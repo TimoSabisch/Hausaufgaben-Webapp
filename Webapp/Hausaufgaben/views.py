dd4324d8463c08f669516bae59d8bbd2d202418a
Timo <88710671+TimoSabisch@users.noreply.github.com>
2021-08-16 10:30:13 +0200

    views.py - added views: menu-,entry-,day- view; urls.py - added urls for views;
    created page-template for script and css links;
    created menu-template as menu (partially functioning);
    created initial html files for menu-,entry-,day- view and the login page;

--- Webapp/Hausaufgaben/views.py
+++ Webapp/Hausaufgaben/views.py
@@ -1,17 +1,76 @@
-from django.shortcuts import render, HttpResponse, redirect
+from django.shortcuts import loader, HttpResponse, redirect
+from django.contrib.auth import login, logout
+from enum import Enum
+
+DEFAULT_USER_ID = 1  # TODO: change to -1
+
+class Views(Enum):
+    WEEK_VIEW = 0
+    ENTRY_VIEW = 1
+    DAY_VIEW = 2
 
 
 def index(request):
-    user_id = request.session.get("user_id", 1)  # TODO: change default id to -1
 
-    return redirect("home" if user_id != -1 else "login")
+    return redirect("home" if request.user.is_authenticated else "login")
 
 
 def home(request):
 
-    return HttpResponse("HOME")
+    return redirect("login" if request.user.is_authenticated else "weekview")
+
+
+def week_view(request):
+    if not request.user.is_authenticated:
+        return redirect("login")
+
+    user = request.user
+    template = loader.get_template("Hausaufgaben/week_view.html")
+
+    groups = user.group_set.all()
+    context = {
+        "groups": groups if len(groups) != 0 else None,
+        "username": user.username
+    }
+
+    return HttpResponse(template.render(context))
+
+
+def entry_view(request):
+    if not request.user.is_authenticated:
+        return redirect("login")
+
+    user = request.user
+    template = loader.get_template("Hausaufgaben/entry_view.html")
+
+    groups = user.group_set.all()
+    context = {
+        "groups": groups if len(groups) != 0 else None,
+        "username": user.username
+    }
+
+    return HttpResponse(template.render(context))
+
+
+def day_view(request):
+    if not request.user.is_authenticated:
+        return redirect("login")
+
+    user = request.user
+    template = loader.get_template("Hausaufgaben/day_view.html")
+
+    groups = user.group_set.all()
+    context = {
+        "groups": groups if len(groups) != 0 else None,
+        "username": user.username
+    }
+
+    return HttpResponse(template.render(context))
 
 
 def login(request):
+    template = loader.get_template("Hausaufgaben/login.html")
 
-    return HttpResponse("LOGIN")
+    # TODO: Add login check
+
+    return HttpResponse(template.render())
