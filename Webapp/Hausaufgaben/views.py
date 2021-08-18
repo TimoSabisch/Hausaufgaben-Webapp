5f6bd8a4ada386de406b7c915b975c0af7ceab73
Timo <88710671+TimoSabisch@users.noreply.github.com>
2021-08-18 12:26:51 +0200

    Added function to convert Entry-Type to string; Added function to get the data needed for the views;

--- Webapp/Hausaufgaben/views.py
+++ Webapp/Hausaufgaben/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import loader, HttpResponse, redirect
 import django.contrib.auth as auth
+import json
+from .models import Group
 from enum import Enum
 
 DEFAULT_USER_ID = 1  # TODO: change to -1
@@ -11,6 +13,16 @@ class Views(Enum):
     DAY_VIEW = 2
 
 
+def entry_type_to_str(entry_type):
+    if entry_type == 0:
+        return "Aufgabe"
+    elif entry_type == 1:
+        return "Erinnerung"
+    elif entry_type == 2:
+        return "Test"
+    return "Unknown"
+
+
 def get_menu_context(request, group):
     user = request.user
 
@@ -20,6 +32,47 @@ def get_menu_context(request, group):
         "groups": groups if len(groups) != 0 else None,
         "username": user.username,
         "currently_viewed": request.session.get("currently_viewed", group if groups.filter(id=group) else 0)
+    }
+
+    return context
+
+
+def get_view_data(request, group):
+    user = request.user
+
+    entries = []
+
+    if group != 0:
+        if Group.objects.filter(id=group):
+            for entry in Group.objects.get(id=group).entries.all():
+                entries.append({
+                    "id": entry.id,
+                    "title": entry.title,
+                    "note": entry.note,
+                    "date": entry.date.isoformat(),
+                    "type": entry_type_to_str(entry.type),
+                    "creator": {
+                        "id": entry.owner.id,
+                        "username": entry.owner.username
+                    }
+                })
+    else:
+        for entry in user.entry_privat_set.all():
+            entries.append({
+                "id": entry.id,
+                "title": entry.title,
+                "note": entry.note,
+                "date": entry.date.isoformat(),
+                "type": entry_type_to_str(entry.type),
+                "creator": {
+                    "id": entry.owner.id,
+                    "username": entry.owner.username
+                }
+            })
+
+    context = {
+        "entries": entries,
+        "user": user.id
     }
 
     return context
@@ -51,6 +104,7 @@ def week_view(request, group=0):
         "view": "weekview"
     }
     context.update(get_menu_context(request, group))
+    context["view_data"] = json.dumps(get_view_data(request, group))
 
     return HttpResponse(template.render(context, request))
 
