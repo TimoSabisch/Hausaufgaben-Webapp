8a357959f89f2eff0edcbec9b06063025194ee9d
Timo <88710671+TimoSabisch@users.noreply.github.com>
2021-08-27 12:00:49 +0200

    Added redirect from root

--- Webapp/Webapp/urls.py
+++ Webapp/Webapp/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/HausaufgabenApp/')),
     path("HausaufgabenApp/", include("Hausaufgaben.urls")),
     path('admin/', admin.site.urls),
 ]
